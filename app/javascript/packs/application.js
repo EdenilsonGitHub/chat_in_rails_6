d60f74684a2f773c212ead9ff87aa5cb224c3102
Edenilson Luiz de Almeida <edenilson@etica-ti.com.br>
2020-11-13 15:51:29 -0300

    Chat ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+module.exports = environment
+$(document).on('turbolinks:load', function () {
+    alert("We made it!");
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :messages
+
+  root "landing#index"
+  
+  get 'landing/index'
+  post 'post' => 'landind#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
